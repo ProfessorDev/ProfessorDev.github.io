996f8650f6b3739727ce8eb86b8594f2895fb53e
tharunpola <tharunpola007@gmail.com>
2020-12-05 23:40:19 +0530

    added discussion storybook

---
+++ precache-manifest.e8e4c194bdcfed232127780b8ee448ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896475fcfec4ac3760c13213bc555b73",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ee53ff2212492550e505.bundle.js"
+  },
+  {
+    "url": "runtime~main.ee53ff2212492550e505.bundle.js"
+  },
+  {
+    "revision": "7c62566ba7fdfdb66830",
+    "url": "static/css/main.cc5481ce.chunk.css"
+  },
+  {
+    "revision": "9c6686d4f3dd3ba1a928",
+    "url": "static/css/vendors~main.7dfb0895.chunk.css"
+  },
+  {
+    "url": "vendors~main.ee53ff2212492550e505.bundle.js"
+  },
+  {
+    "url": "vendors~main.ee53ff2212492550e505.bundle.js.LICENSE.txt"
+  }
+]);
